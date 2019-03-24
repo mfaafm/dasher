0fc43661c55fdeca224f8897f8a1a79733740a00
Martijn Arts <m.arts@gmx.net>
2019-03-25 00:07:29 +0100

    Update demo app.

--- my_app.py
+++ my_app.py
@@ -1,12 +1,49 @@
+import pandas as pd
+import numpy as np
 from dasher import Dasher
 import dash_html_components as html
 import dash_core_components as dcc
+import plotly.graph_objs as go
 
-app = Dasher(__name__, title="My dashboard")
+app = Dasher(__name__, title="My fancy dashboard")
+
+
+df = pd.DataFrame(
+    {
+        "x": np.arange(3),
+        "a": np.arange(1, 4),
+        "b": np.array([2, -2, 12]),
+        "c": np.array([10, 0, -10]),
+    }
+)
+
+
+@app.callback("Line plot", column=df.columns.drop("x"))
+def line_plot(column):
+    return [dcc.Graph(figure={"data": [{"x": df.x, "y": df[column]}]})]
+
+
+@app.callback("Bar plot", column=df.columns.drop("x"))
+def bar_plot(column):
+    return [
+        dcc.Graph(
+            figure=go.Figure(
+                data=[
+                    go.Bar(
+                        x=df.x,
+                        y=df[column],
+                        name="bars",
+                        marker=go.bar.Marker(color="rgb(55, 83, 109)"),
+                    )
+                ]
+            )
+        )
+    ]
 
 
 @app.callback(
-    "First",
+    "All widgets",
+    "Demo of all supported automatic widgets",
     a=True,
     b="Test",
     c=["apple", "orange", "lemon", "banana"],
@@ -20,24 +57,6 @@ app = Dasher(__name__, title="My dashboard")
     m=dcc.RangeSlider(count=1, min=-5, max=10, step=0.5, value=[-3, 7]),
 )
 def my_function(*args):
-    text = " ".join(
-        ["{}: {} ({})".format(i, v, str(type(v))) for i, v in enumerate(args)]
-    )
-    return [html.Label(text)]
-
-
-@app.callback(
-    "Second", "Super nice content!", a=True, b=["apple", "orange", "lemon", "banana"]
-)
-def my_second_function(*args):
-    text = " ".join(
-        ["{}: {} ({})".format(i, v, str(type(v))) for i, v in enumerate(args)]
-    )
-    return [html.Label(text)]
-
-
-@app.callback("Third", a=True, b=["apple", "orange", "lemon", "banana"])
-def my_third_function(*args):
     text = " ".join(
         ["{}: {} ({})".format(i, v, str(type(v))) for i, v in enumerate(args)]
     )
